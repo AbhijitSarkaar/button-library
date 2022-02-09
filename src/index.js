0da7a351b0872c5ff813b9330b72af7d1ad26ad0
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-02-09 19:29:53 +0530

    webpack config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div>Hello world using React and ReactDOM </div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
