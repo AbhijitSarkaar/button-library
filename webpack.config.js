94697f2928c73a27bbb56abc4a4505a78a60621f
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-02-11 13:28:41 +0530

    server setup and webpack config changes for html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -16,4 +17,7 @@ module.exports = {
             },
         ],
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })]
 };
